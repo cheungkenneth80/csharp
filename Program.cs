78a8c6e2edab56c9d3c1762856d874ef4aa44f07
Kenneth Cheung <meetbeateat@outlook.com>
2020-01-14 02:26:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+static void Main1()
+    {
+        Console.WriteLine("Hello, World!1");
+    }
+
+static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Main1();
+    }
+}
+
